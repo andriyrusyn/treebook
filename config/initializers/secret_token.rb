f8a919eaebcdb1829da135e096a210f4039e2110
Andriy Rusyn <rusyn.andriy@gmail.com>
2014-02-27 01:52:35 +0000

    hello this is a test from treehouse ruby lesson

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '16f96c925b98263f0beddd351bf4e0b78223a6a769d8c33dd7741c99508e6c2d2d14c10e97f3a68a4fab94bc3bbf26ebc02855c01983056277506bd1948403b8'
